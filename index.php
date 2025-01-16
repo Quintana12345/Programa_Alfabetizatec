55fac8d85670f9ae43df71570f916bde78b3ec06
Eliezer Martinez <develiezermartinez@gmail.com>
2025-01-15 22:28:46 -0600

    incorporacion de archivos y assets

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>TecNM - INEA Alfabetizatec</title>
+    <link rel="shortcut icon" href="./assets/img/favicon/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="./assets/css/index.css">
+
 </head>
+
 <body>
-    
+    <header class="principal_header">
+        <img src="./assets/img/logotipos/sep.png" alt="Logotipo SEP">
+        <img src="./assets/img/logotipos/TecNM_Azteca.png" alt="Logotipo TECNM">
+        <img src="./assets/img/logotipos/inea_blanco.png" alt="Logotipo INEA">
+    </header>
+    <main>
+        <img src="./assets/img/logotipos/alfabetizatec_logo.png" id="LogoAlfabetizatec" alt="Alfabetitec logotipo">
+        Programa Nacional Alfabetizatec
+    </main>
+    <footer></footer>
 </body>
+
 </html>
