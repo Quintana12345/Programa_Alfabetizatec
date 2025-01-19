d74f1b886fc240313de3c2158e0bf49a969dafd1
Eliezer Martinez <develiezermartinez@gmail.com>
2025-01-18 21:16:09 -0600

    yimmi

--- src/login.php
+++ src/login.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ALFABETIZATEC - Login</title>
-    <link rel="stylesheet" href="../assets/css/login.css">
     <link rel="stylesheet" href="../assets/css/root.css">
+    <link rel="stylesheet" href="../assets/css/login.css">
 
 </head>
 
